74912a908d4aa5ff398cd86a7dcf3cca332a6b04
Weuller Jr. Souza <weuller9505@live.com>
2018-06-05 22:00:54 -0300

    Layout Variável

--- js/gamepad.js
+++ js/gamepad.js
@@ -1,45 +1,91 @@
-var gamepads, gb,
+var gamepads, gp,
     Laxes = [ 2 ],
     Raxes = [ 2 ],
-    LsPoint, RsPoint;
-var LstickPosition = document.getElementById('Lstick');
-var LstickInfo = document.getElementById('LstickInfo');
-var RstickPosition = document.getElementById('Rstick');
-var RstickInfo = document.getElementById('RstickInfo');
-var gamepadInfo = document.getElementById('controlInfo');
-var controlInfo = document.getElementById('Info');
+    visualPadList = new Array();
+var grid = document.getElementById('GPgrid');
 
 window.addEventListener('gamepadconnected', createPadUI);
 window.addEventListener('load', windowLoaded);
+
+function newElement (tipo, classe, id) {
+    var el = document.createElement(tipo);
+    if (classe != undefined) {
+        el.setAttribute('class', classe);
+    }
+    if (id != undefined) {
+        el.setAttribute('id', 'n' + id);
+    }
+    return el;
+}
+
+function visualPad (gp, n) {
+    this.gamepadUI = newElement('table', 'Gamepad', n);
+    grid.appendChild(this.gamepadUI);
+
+    //Linha 1 ( L(x/y) botao pressionado R(x/y))
+    this.gamepadInfo = newElement('tr', 'GamepadInfo', n);  //// BUG: provavel bug no layout
+    if (gp.axes != undefined) { //caso tenha analógicos
+        this.LstickInfo = newElement('td', 'LstickInfo', n);
+        this.gamepadInfo.appendChild(this.LstickInfo);
+    }
+    this.info = newElement('td', 'Info', n);
+    this.gamepadInfo.appendChild(this.info);
+    if (gp.axes != undefined) { //caso tenha analógicos
+        this.RstickInfo = newElement('td', 'RstickInfo', n);
+        this.gamepadInfo.appendChild(this.RstickInfo);
+    }
+    this.gamepadUI.appendChild(this.gamepadInfo);
+
+    //Linha 2 ( L(pos) info Gamepad R(pos))
+    this.extraInfo = newElement('tr', 'ExtraInfo', n);
+    if (gp.axes != undefined) { //caso tenha analógicos
+        this.Lstick = newElement('td', 'Lstick', n);
+        this.extraInfo.appendChild(this.Lstick);
+
+        this.LsPoint = document.createElement('div');
+        this.LsPoint.style.width = '10px';
+        this.LsPoint.style.height = '10px';
+        this.LsPoint.style.position = 'relative';
+        this.LsPoint.style.left = (this.Lstick.clientWidth / 2) + 'px';
+        this.LsPoint.style.top = (this.Lstick.clientHeight / 2) + 'px';
+        this.LsPoint.style.backgroundColor = 'cyan';
+        this.Lstick.style.border = '1px solid cyan';
+        this.Lstick.appendChild(this.LsPoint);
+    }
+    this.controlInfo = newElement('td', 'controlInfo', n);
+    this.extraInfo.appendChild(this.controlInfo);
+    if (gp.axes != undefined) { //caso tenha analógicos
+        this.Rstick = newElement('td', 'Rstick', n);
+        this.extraInfo.appendChild(this.Rstick);
+
+        this.RsPoint = document.createElement('div');
+        this.RsPoint.style.position = 'relative';
+        this.RsPoint.style.left = (this.Rstick.clientWidth / 2) + 'px';
+        this.RsPoint.style.top = (this.Rstick.clientHeight / 2) + 'px';
+        this.RsPoint.style.width = '10px';
+        this.RsPoint.style.height = '10px';
+        this.RsPoint.style.backgroundColor = 'red';
+        this.Rstick.style.border = '1px solid red';
+        this.Rstick.appendChild(this.RsPoint);
+    }
+    this.gamepadUI.appendChild(this.extraInfo);
+}
 
 function windowLoaded () {
     setInterval(pollGamepads, 60);
 }
 window.addEventListener('gamepaddisconnected', function () {
-    controlInfo.innerHTML = 'Waiting for gamepad.';
+    grid.innerHTML = 'Waiting for gamepad.';
 });
 
 function createPadUI () {
-//cria os pontos dos analógicos
-    LsPoint = document.createElement('div');
-    LsPoint.style.width = '10px';
-    LsPoint.style.height = '10px';
-    LsPoint.style.position = 'relative';
-    LsPoint.style.left = (LstickPosition.clientWidth / 2) + 'px';
-    LsPoint.style.top = (LstickPosition.clientHeight / 2) + 'px';
-    LsPoint.style.backgroundColor = 'cyan';
-    LstickPosition.style.border = '1px solid cyan';
-    LstickPosition.appendChild(LsPoint);
-
-    RsPoint = document.createElement('div');
-    RsPoint.style.position = 'relative';
-    RsPoint.style.left = (RstickPosition.clientWidth / 2) + 'px';
-    RsPoint.style.top = (RstickPosition.clientHeight / 2) + 'px';
-    RsPoint.style.width = '10px';
-    RsPoint.style.height = '10px';
-    RsPoint.style.backgroundColor = 'red';
-    RstickPosition.style.border = '1px solid red';
-    RstickPosition.appendChild(RsPoint);
+    let nGp,
+        i = 0;
+    do {
+        let vp = new visualPad(gamepads[i], i);
+        visualPadList.push(vp);
+        i++;
+    } while (gamepads[i] != undefined);
 
     pollGamepads();
     gamepadLoop();
@@ -54,13 +100,13 @@ function pollGamepads () {
     for (var i = 0; i < gamepads.length; i++) {
         gp = gamepads[i];
         if (gp) {
-            gamepadInfo.innerHTML = '<br>' + 'Index ' + gp.index + ': ' + gp.id + ' ' + gp.buttons.length + ' buttons and ' + gp.axes.length + ' axes.';
-            gamepadLoop();
+            visualPadList[i].controlInfo.innerHTML = '<br>' + 'Index ' + gp.index + ': ' + gp.id + ' ' + gp.buttons.length + ' buttons and ' + gp.axes.length + ' axes.';
+            gamepadLoop(visualPadList[i], i);
             clearInterval(interval);
         }
     }
 }
-setInterval(gamepadLoop, 60);
+// setInterval(gamepadLoop, 60);
 
 function buttonPressed (b) {
     if (b.pressed == true) {
@@ -68,101 +114,101 @@ function buttonPressed (b) {
     }
 }
 
-function checkAxes (L = 2, R = 2) {
+function checkAxes (L = 2, R = 2, i) {
     var t = 0;
     //L
     if (L[0] > 0) {   //x
-        LsPoint.style.left = (75 + (5 * L[0])) + 'px';
+        visualPadList[i].LsPoint.style.left = (75 + (5 * L[0])) + 'px';
         //dir
     } else {
-        LsPoint.style.left = (75 + (5 * L[0])) + 'px';
+        visualPadList[i].LsPoint.style.left = (75 + (5 * L[0])) + 'px';
         //esq
     }
     if (L[1] > 0) {   //y
-        LsPoint.style.top = (75 + (5 * L[1])) + 'px';
+        visualPadList[i].LsPoint.style.top = (75 + (5 * L[1])) + 'px';
         //des
     } else {
-        LsPoint.style.top = (75 + (5 * L[1])) + 'px';
+        visualPadList[i].LsPoint.style.top = (75 + (5 * L[1])) + 'px';
         //sob
     }
     //-------------------------------------------------------
     //R
     if (R[0] > 0) {   //x
-        RsPoint.style.left = (75 + (5 * R[0])) + 'px';
+        visualPadList[i].RsPoint.style.left = (75 + (5 * R[0])) + 'px';
         //dir
     } else {
-        RsPoint.style.left = (75 + (5 * R[0])) + 'px';
+        visualPadList[i].RsPoint.style.left = (75 + (5 * R[0])) + 'px';
         //esq
     }
     if (R[1] > 0) {   //y
-        RsPoint.style.top = (75 + (5 * R[1])) + 'px';
+        visualPadList[i].RsPoint.style.top = (75 + (5 * R[1])) + 'px';
         //des
     } else {
-        RsPoint.style.top = (75 + (5 * R[1])) + 'px';
+        visualPadList[i].RsPoint.style.top = (75 + (5 * R[1])) + 'px';
         //sob
     }
 }
 
-function gamepadLoop () {
+function gamepadLoop (agp, i) {
     try {
-        if (gamepads[0] == undefined) { return; }
+        if (gamepads[i] == undefined) { return; }
     } catch (e) {
         return;
     }
 
-    gp = gamepads[0]; //armazena em 'gp' o gamepad ativo
-    controlInfo.innerHTML = '';
+    gp = gamepads[i]; //armazena em 'gp' o gamepad ativo
+    agp.info.innerHTML = '';
     //A X Y B
     if (buttonPressed(gp.buttons[0])) {
-        controlInfo.innerHTML += 'A<br>';
+        agp.info.innerHTML += 'A';
     }
     if (buttonPressed(gp.buttons[1])) {
-        controlInfo.innerHTML += 'B<br>';
+        agp.info.innerHTML += 'B ';
     }
     if (buttonPressed(gp.buttons[2])) {
-        controlInfo.innerHTML += 'X<br>';
+        agp.info.innerHTML += 'X ';
     }
     if (buttonPressed(gp.buttons[3])) {
-        controlInfo.innerHTML += 'Y<br>';
+        agp.info.innerHTML += 'Y ';
     }
     // L R
     if (buttonPressed(gp.buttons[4])) {
-        controlInfo.innerHTML += 'LB<br>';
+        agp.info.innerHTML += 'LB ';
     }
     if (buttonPressed(gp.buttons[6])) {
-        controlInfo.innerHTML += 'LT<br>';
+        agp.info.innerHTML += 'LT ';
     }
     if (buttonPressed(gp.buttons[5])) {
-        controlInfo.innerHTML += 'RB<br>';
+        agp.info.innerHTML += 'RB ';
     }
     if (buttonPressed(gp.buttons[7])) {
-        controlInfo.innerHTML += 'RT<br>';
+        agp.info.innerHTML += 'RT ';
     }
 
     if (buttonPressed(gp.buttons[8])) {
-        controlInfo.innerHTML += 'back<br>';
+        agp.info.innerHTML += 'back ';
     }
     if (buttonPressed(gp.buttons[9])) {
-        controlInfo.innerHTML += 'start<br>';
+        agp.info.innerHTML += 'start ';
     }
     if (buttonPressed(gp.buttons[10])) {
-        controlInfo.innerHTML += 'analog Lb<br>';
+        agp.info.innerHTML += 'analog L ';
     }
     if (buttonPressed(gp.buttons[11])) {
-        controlInfo.innerHTML += 'analog Rb<br>';
+        agp.info.innerHTML += 'analog R ';
     }
     if (buttonPressed(gp.buttons[12])) {
-        controlInfo.innerHTML += 'cima<br>';
+        agp.info.innerHTML += 'up ';
     }
     if (buttonPressed(gp.buttons[13])) {
-        controlInfo.innerHTML += 'baixo<br>';
+        agp.info.innerHTML += 'down ';
     }
     if (buttonPressed(gp.buttons[14])) {
-        controlInfo.innerHTML += 'esquerda<br>';
+        agp.info.innerHTML += 'left ';
     }
     if (buttonPressed(gp.buttons[15])) {
         // alert('direita');
-        controlInfo.innerHTML += 'direita<br>';
+        agp.info.innerHTML += 'right ';
     }
 
     Laxes[0] = Math.round(10 * gp.axes[0]);       // L x
@@ -170,8 +216,8 @@ function gamepadLoop () {
 
     Raxes[0] = Math.round(10 * gp.axes[2]);       //R x
     Raxes[1] = Math.round(10 * gp.axes[3]);       //R y
-    checkAxes(Laxes, Raxes);
+    checkAxes(Laxes, Raxes, i);
 
-    LstickInfo.innerHTML = 'x: ' + Laxes[0] + ' y: ' + Laxes[1];
-    RstickInfo.innerHTML = 'x: ' + Raxes[0] + ' y: ' + Raxes[1];
+    agp.LstickInfo.innerHTML = 'x: ' + Laxes[0] + ' y: ' + Laxes[1];
+    agp.RstickInfo.innerHTML = 'x: ' + Raxes[0] + ' y: ' + Raxes[1];
 }
